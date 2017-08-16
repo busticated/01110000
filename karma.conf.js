c129ecc226a0c37daf9614e8c204c0beebfebc8e
busticated <itsbusticated@gmail.com>
2017-08-15 22:52:41 -0700

    add karma browser test runner and related tooling

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(cfg){
+    cfg.set({
+        basePath: '',
+        frameworks: ['mocha', 'sinon', 'chai'],
+        files: ['src/client/**/*.test.js'],
+        exclude: ['.git', 'node_modules'],
+        preprocessors: {
+            'src/client/**/*.test.js': ['webpack', 'sourcemap']
+        },
+        webpackPort: 9877,
+        webpack: { devtool: 'eval' },
+        plugins: [
+            'karma-mocha',
+            'karma-mocha-reporter',
+            'karma-sinon',
+            'karma-chai',
+            'karma-webpack',
+            'karma-sourcemap-loader',
+            'karma-chrome-launcher'
+        ],
+        reporters: ['mocha'],
+        mochaReporter: { showDiff: true },
+        port: 9876,
+        transports: ['polling', 'websocket'],
+        colors: true,
+        logLevel: cfg.LOG_INFO,
+        autoWatch: true,
+        autoWatchBatchDelay: 1000,
+        browsers: ['Chrome'],
+        browserNoActivityTimeout: 60000,
+        singleRun: false
+    });
+};
